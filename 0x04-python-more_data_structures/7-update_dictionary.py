bfa3f9880e6cc83d68a2e1c3089ba885affda917
Joroyce <joroyce2412@gmail.com>
2022-09-28 14:03:00 -0700

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def simple_delete(my_dict, key=""):
-    if key in my_dict:
-        del my_dict[key]
+def update_dictionary(my_dict, key, value):
+    my_dict[key] = value
     return my_dict
