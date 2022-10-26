ce0b58c3bb5a2a4087c17a698d70c14d68ebb8ff
Joroyce <joroyce2412@gmail.com>
2022-10-25 18:30:28 -0700

    0-read_file.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+contains the json str function
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """returns an object represented by a JSON string"""
+    return json.loads(my_str)
+i
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+this is 0x0B-python-input_output
